9cde0a55325de6699421e10a6c8560f251406f17
Cory Alder <cory@buddybuild.com>
2017-08-22 13:05:28 -0700

    Initial Commit

---
+++ DummyProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DummyProj
+//
+//  Created by Cory Alder on 2017-08-22.
+//  Copyright © 2017 Cory Alder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DummyProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DummyProj
+//
+//  Created by Cory Alder on 2017-08-22.
+//  Copyright © 2017 Cory Alder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
